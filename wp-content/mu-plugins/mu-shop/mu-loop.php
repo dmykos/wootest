bb72aae9ed22d04f034a5ecd803de91ed4ea3717
Dmytro Kosto <dmykos@gmail.com>
2018-02-14 19:58:34 +0200

    Add mu plugins

---
+++ wp-content/mu-plugins/mu-shop/mu-loop.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:       Mu Shop
+ * Description:       Must Use Shop plugin
+ * Version:           1.0.0
+ * Text Domain:       mu-shop
+ */
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
