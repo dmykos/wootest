411b9c75fe9ecc7e1bf6490ab833851043b0a98b
Dmytro Kosto <dmykos@gmail.com>
2018-02-15 20:49:04 +0200

    Show subtitle on frontend

--- wp-content/mu-plugins/mu-shop/mu-loop.php
+++ wp-content/mu-plugins/mu-shop/mu-loop.php
@@ -17,8 +17,22 @@ function wootest_init(){
 wootest_init();
 
 
+
+
 // Show selected movies after login
 add_filter( 'login_redirect', function( $url, $query, $user ) {
     return get_term_link( 'best', 'movie_category' );
 }, 10, 3 );
+
+function property_slideshow( $content ) {
+    if ( is_single() && 'movie' == get_post_type() ) {
+        $movie_subtitle = get_post_meta( get_the_ID(), 'movie_subtitle', 1 );
+        $custom_content = '<h2>'.$movie_subtitle.'</h2>';
+        $custom_content .= $content;
+        return $custom_content;
+    } else {
+        return $content;
+    }
+}
+add_filter( 'the_content', 'property_slideshow' );
 
