95384809cc09b037d61e2bf5cd8e58227ba37e59
Dmytro Kosto <dmykos@gmail.com>
2018-02-22 08:59:54 +0200

    Show add to cart on movie page

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -96,3 +96,21 @@ function reigel_woocommerce_get_price($price,$product){
 
 }
 add_filter( 'woocommerce_product_class', 'wc_product_class', 10, 3);*/
+
+
+ add_filter('the_content','rei_add_to_cart_button', 20,1);
+ function rei_add_to_cart_button($content){
+     global $post;
+     if ($post->post_type !== 'movie') {return $content; }
+
+     ob_start();
+     ?>
+     <form action="" method="post">
+         <input name="add-to-cart" type="hidden" value="<?php echo $post->ID ?>" />
+         <input name="quantity" type="number" value="1" min="1"  />
+         <input name="submit" type="submit" value="Add to cart" />
+     </form>
+     <?php
+
+     return $content . ob_get_clean();
+ }
