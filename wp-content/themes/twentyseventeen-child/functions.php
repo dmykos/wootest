e52a20b39a94d2249742fbafb73ee45561670b4c
Dmytro Kosto <dmykos@gmail.com>
2018-02-22 08:51:13 +0200

    fix notice

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -80,9 +80,10 @@ function woocommerce_product_get_price( $price, $product ) {
 }*/
 
 add_filter('woocommerce_product_get_price','reigel_woocommerce_get_price',20,2);
-function reigel_woocommerce_get_price($price,$post){
-    if ($post->post->post_type === 'movie')
-        $price = get_post_meta($post->id, "price", true);
+function reigel_woocommerce_get_price($price,$product){
+    if ( get_post_type( $product->get_id() ) === 'movie'){
+        $price = get_post_meta($product->get_id(), "price", true);
+    }
     return $price;
 }
 
